65b6b353fd0a3a626191caec3b7720833f74c5a8
kamalhlc73 <128677681+kamalhlc73@users.noreply.github.com>
2023-05-06 21:33:02 +0530

    Add files via upload

---
+++ assets/php/Config.php
@@ -0,0 +1,21 @@
+<?php
+
+$server='localhost';
+$username='admin';
+$password='Qwertyuiop@123';
+$database=' id19830405_kamalkp27';
+
+$con= mysqli_connect($server, $username, $password, $database);
+ $db= mysqli_select_db($con, $database);
+
+
+if($con){
+    "<script>
+    alert('Submit')
+    ";
+}else{
+    echo "failed";
+}
+
+
+?>
